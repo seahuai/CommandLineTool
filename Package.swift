0fbebce44a4f776959d89fefae13228af01840cb
zhangsihuai <727087311@qq.com>
2017-09-04 13:27:34 +0800

    add Rainbow

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ import PackageDescription
 let package = Package(
     name: "CommandLineTool",
     dependencies: [
-        .Package(url: "https://github.com/jatoben/CommandLine", "3.0.0-pre1" )
+        .Package(url: "https://github.com/jatoben/CommandLine", "3.0.0-pre1" ),
+        .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 2)
     ]
     
 )
